ce6c6a3adbc4f183d6e6730a25b120b8136ce7ef
gaurangrshah <gaurang.r.shah@gmail.com>
2019-05-30 03:43:51 -0400

    sets up site details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: "/gatsby-react-bootstrap-starter",
   siteMetadata: {
-    title: `Gatsby React Bootstrap Starter`,
-    description: `A starter that includes react-bootstrap and react-icons, along with SASS compilation.`,
-    author: `Billy Jacoby`,
+    title: `Think Mechanic (beta)`,
+    description: `Coming Soon.`,
+    author: `Think Mechanic`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
